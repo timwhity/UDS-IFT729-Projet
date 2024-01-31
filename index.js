23d244369d0f596b510a915d05bb816f56f80469
tymitri <tiwhity@laposte.net>
2024-01-31 14:49:06 -0500

    Première version tableau blanc côté client

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
 	// Traitement sur l'url, sur les cookies, ... 
 	  res.render('index');
 });
 
-app.set('view engine', 'ejs');
 
 app.listen(port, () => {
 	  console.log(`Example app listening at http://localhost:${port}`);
