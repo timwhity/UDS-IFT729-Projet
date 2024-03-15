7eec63f1333df1f6cbf7ed2f6713fed1608e6106
ndounalex <ndounalex@yahoo.fr>
2024-03-15 16:33:23 -0400

    correction d'un bug sur la creation du textarea

--- public/canvasManager.js
+++ public/canvasManager.js
@@ -225,7 +225,8 @@ class CanvasManager {
         var text = new fabric.Textbox('',  
             { 
                 width: 450,
-                height:30
+                height:30,
+                id: this.genId()
             });
         return text;
     }
